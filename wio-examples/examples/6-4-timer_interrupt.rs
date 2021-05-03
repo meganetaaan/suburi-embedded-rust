549ee0dd307d30844c89b1d3f7302d6c5416876c
Shinya Ishikawa <ishikawa.s.1027@gmail.com>
2021-05-03 06:55:57 +0000

    Add timer_interrupt

--- wio-examples/examples/6-4-timer_interrupt.rs
+++ wio-examples/examples/6-4-timer_interrupt.rs
@@ -51,21 +51,50 @@ fn main() -> ! {
     );
 
     // TODO: 2MHzのクロックを取得する
+    let gclk5 = clocks.get_gclk(wio::pac::gclk::pchctrl::GEN_A::GCLK5).unwrap();
 
     // TODO: TC3へのクロックを2MHzにする
+    let timer_clock = clocks.tc2_tc3(&gclk5).unwrap();
 
     // TODO: TC3ドライバオブジェクトを初期化する
+    let mut tc3 = TimerCounter::tc3_(
+        &timer_clock,
+        peripherals.TC3,
+        &mut peripherals.MCLK
+    );
 
     // TODO: 割り込みコントローラで、TC3の割り込み通知を有効化する
+    unsafe {
+        NVIC::unmask(interrupt::TC3);
+    }
 
     // TODO: 1秒のカウントを開始して、TC3が割り込みが発生するようにする
+    tc3.start(1.s());
+    tc3.enable_interrupt();
 
     // TODO: 割り込みハンドラと共有するリソースを格納する
+    unsafe {
+        CTX = Some(Ctx {
+            led: Led::new(sets.user_led, &mut sets.port),
+            tc3,
+        })
+    }
 
     // TODO: シリアルターミナルにechoし続ける
     loop {
-        
+        if let Ok(c) = nb::block!(serial.read()) {
+            nb::block!(serial.write(c)).unwrap();
+        }
     }
 }
 
 // TODO: TC3 の割り込みハンドラを実装する
+#[interrupt]
+fn TC3() {
+    // 割り込みハンドラで行う処理
+    unsafe {
+        let ctx = CTX.as_mut().unwrap();
+        ctx.tc3.wait().unwrap();
+        ctx.led.toggle();
+    }
+}
